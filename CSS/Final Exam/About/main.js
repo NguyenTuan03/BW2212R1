a7de65dedfc1ceccc416e3663fe3d636bdede8ef
Tuan Nguyen <tuannase170164@fpt.edu.vn>
2023-03-23 15:41:11 +0700

    add

---
+++ CSS/Final Exam/About/main.js
@@ -0,0 +1,83 @@
+document.querySelector('.menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('open');
+    document.querySelector('.header-menu').classList.toggle('d-move');
+}
+document.querySelector('.Backtotop').onclick = function() {
+    window.scrollTo(0,0);
+}
+$(document).ready(function() {
+
+    $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.container");
+        var pos = event.target.value;
+        element.find("div.before").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+    });
+});
+var lastScrollTop = 0;
+navbar = document.querySelector('.scroll-navbar');
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) 
+    {
+        navbar.style.top = '-120px';
+        navbar.style.transition = 'all ease-in 0.2s';
+    }
+    else
+    {
+        navbar.style.top = '0px';
+        navbar.style.transition = 'all ease-in 0.2s';
+    }
+    lastScrollTop = scrollTop;
+});
+
+window.addEventListener('scroll',reveal);
+function reveal()
+{
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i=0;i<reveals.length;i++)
+    {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+        if (revealTop < windowHeight - revealPoint)
+        {
+            reveals[i].classList.add('active');
+        }
+        else
+        {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+var rights = document.querySelectorAll('.right_animate');
+var lefts = document.querySelectorAll('.left_animate');
+window.addEventListener('scroll', slideIn)
+function slideIn()
+{
+    const triggerBottom = window.innerHeight/5*4;
+    rights.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom)
+        {
+            box.classList.add('active');
+        }
+        else
+        {
+            box.classList.remove('active');
+        }
+    })
+    const leftBottm = window.innerHeight/5*4;
+    lefts.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < leftBottm)
+        {
+            box.classList.add('active');
+        }
+        else
+        {
+            box.classList.remove('active');
+        }
+    })
+}
